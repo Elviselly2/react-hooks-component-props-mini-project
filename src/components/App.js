2e1c822d1fe54fe2586f4a0d0f554537bb300eb9
elvis <elvisotieno24@gmail.com>
2025-04-10 01:26:22 +0300

    adds files

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import blogData from "../data/blog";
-
+import Header from "./Header";
+import About from "./About";
+import ArticleList from "./ArticleList";
+import Article from "./Article"
 console.log(blogData);
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      {/* You're on your own from here! Follow the deliverables; test things out in
+      the browser as you write your code; and good luck! */}
+       <Header title="Underreacted" />
+      <About 
+        image="data:image/png;base64,..."
+        about="A blog about learning React"
+      />
+      <ArticleList posts={blogData.posts} />
     </div>
   );
 }
